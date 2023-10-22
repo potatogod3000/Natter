c53de1e4f753560b9068e1b18562760d148ea852
Samuel Ramesh <sampingu02@gmail.com>
2023-10-22 23:51:02 +0530

    Add and setup Tailwind CSS with helper packages

--- NatterClient/src/main.js
+++ NatterClient/src/main.js
@@ -1,8 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+import './assets/base.css'
 
 const app = createApp(App)
 
