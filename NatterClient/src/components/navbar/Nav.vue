9ea461c6899f9def617a1ccabe4cd47f8d62843d
Samuel Ramesh <sampingu02@gmail.com>
2023-10-27 23:31:55 +0530

    Add navbar

---
+++ NatterClient/src/components/navbar/Nav.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+
+</script>
+
+<template>
+    <header class="dark:bg-darkBlue dark:text-lightBlueShadow bg-lightBlueShadow shadow-sm shadow-darkBlue dark:shadow-lightBlueShadow font-bold">
+        <ul class="container mx-auto">
+            <div class="flex h-14 items-center justify-between">
+                <li><RouterLink to="/"><img src="/favicon.ico" width="32" /></RouterLink></li>
+                
+                <div class="flex gap-8">
+                    <li><RouterLink to="/about" class="hover:underline underline-offset-8 transition-all">About</RouterLink></li>
+                    <li><RouterLink to="/chats" class="hover:underline underline-offset-8 transition-all">Natter Chat</RouterLink></li>
+                </div>
+                
+                <li><RouterLink to="/auth" class="hover:underline underline-offset-8 transition-all">Login / Register</RouterLink></li>
+            </div>
+        </ul>
+    </header>
+</template>
---
+++ NatterClient/src/components/navbar/NavFooter.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="footer">
+        <p>2023 &copy;Natter</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
