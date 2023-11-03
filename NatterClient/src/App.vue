8cc3fdcbc15c7659c3c79fe668b81e3f96bffb9c
Samuel Ramesh <sampingu02@gmail.com>
2023-11-03 23:32:20 +0530

    Make footer stick to bottom of the page

--- NatterClient/src/App.vue
+++ NatterClient/src/App.vue
@@ -5,9 +5,13 @@ import NavFooter from './components/navbar/NavFooter.vue'
 </script>
 
 <template>
-  <Nav v-if="!$route.meta.hideNav" />
+  <main class="flex flex-col min-h-screen">
+    <Nav v-if="!$route.meta.hideNav" />
 
-  <RouterView />
+    <section class="flex-grow">
+      <RouterView />
+    </section>
 
-  <NavFooter v-if="!$route.meta.hideNav" />
+    <NavFooter v-if="!$route.meta.hideNav" />
+  </main>
 </template>
