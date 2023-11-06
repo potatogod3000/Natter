a845780a7e97302779c606300c98fcbd4262960c
Samuel Ramesh <sampingu02@gmail.com>
2023-11-06 23:57:32 +0530

    Add Dark Mode toggle functionality in front end

---
+++ NatterClient/src/components/navbar/ToggleDarkMode.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core';
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+    <div class="ml-8 cursor-pointer" @click="toggleDark()">
+        {{ isDark ? "Dark" : "Light" }}
+    </div>
+</template>
