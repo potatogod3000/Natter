b2e8fbcf7594106ce79548bb9e562161b6f38756
Samuel Ramesh <sampingu02@gmail.com>
2023-11-08 21:32:10 +0530

    Navigation Bars (Header & Footer) improvement

--- NatterClient/src/components/navbar/Nav.vue
+++ NatterClient/src/components/navbar/Nav.vue
@@ -1,41 +1,68 @@
 <script setup>
-import { RouterLink } from 'vue-router';
-import { useUserStore } from '@/stores/userStore'
+import { RouterLink } from "vue-router";
+import { useUserStore } from '@/stores/userStore';
+import ToggleDarkMode from '@/components/navbar/ToggleDarkMode.vue'
 
-const userStore = useUserStore()
+const userStore = useUserStore();
+
+let prevScrollPos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollPos = currentScrollPos;
+};
 </script>
 
 <template>
-    <header class="fixed top-0 left-0 right-0 z-10 bg-light-soft/95 dark:bg-dark-soft/95 filter backdrop-blur">
-        <nav class="container py-2">
-            <ul>
-                <div class="flex items-center justify-between font-bold text-black dark:text-white">
-                    <li>
-                        <RouterLink :to="{name: 'home'}" class="flex items-center gap-1">
-                            <img src="/favicon.ico" width="32" />
-                            <span class="text-lg">Natter</span>
-                        </RouterLink>
-                    </li>
-                    
-                    <div class="flex gap-8">
+  <header
+    id="navbar"
+    class="fixed top-0 left-0 right-0 z-10 bg-light-soft/90 dark:bg-dark-soft/80 shadow-sm shadow-dark-soft/20 dark:shadow-light-soft/20 filter backdrop-blur transition-all duration-500"
+  >
+    <nav class="container py-2">
+      <ul>
+        <div
+          class="flex items-center justify-between text-dark dark:text-light font-bold"
+        >
+          <li>
+            <RouterLink :to="{ name: 'home' }" class="flex gap-1">
+              <img src="/favicon.ico" width="32" />
+              <span class="font-palanquin text-lg text-accent">Natter</span>
+            </RouterLink>
+          </li>
 
-                        <li>
-                            <RouterLink :to="{name: 'about'}" class="hover:underline underline-offset-8 transition-all duration-200">About</RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink :to="{name: 'chats'}" class="hover:underline underline-offset-8 transition-all duration-200">Natter Chat</RouterLink>
-                        </li>
+          <div class="flex gap-8">
+            <li>
+              <RouterLink
+                :to="{ name: 'chats' }"
+                class="hover:underline underline-offset-8 transition-all duration-200"
+                >Natter Chat</RouterLink
+              >
+            </li>
 
-                        <li v-if="!userStore.isLoggedIn">
-                            <RouterLink :to="{name: 'auth'}" class="hover:underline underline-offset-8 transition-all duration-200">Login / Register</RouterLink>
-                        </li>
-                        <li v-else>
-                            <RouterLink :to="{name: 'profile'}" class="hover:underline underline-offset-8 transition-all duration-200">Hello, {{ userStore.username }}</RouterLink>
-                        </li>
-                    
-                    </div>
-                </div>
-            </ul>
-        </nav>
-    </header>
+            <li v-if="!userStore.isLoggedIn">
+              <RouterLink
+                :to="{ name: 'auth' }"
+                class="hover:underline underline-offset-8 transition-all duration-200"
+                >Login / Register</RouterLink
+              >
+            </li>
+            <li v-else>
+              <RouterLink
+                :to="{ name: 'profile' }"
+                class="hover:underline underline-offset-8 transition-all duration-200"
+                >Hello, {{ userStore.username }}</RouterLink
+              >
+            </li>
+            <li class="border-l border-black dark:border-white pl-8">
+              <ToggleDarkMode />
+            </li>
+          </div>
+        </div>
+      </ul>
+    </nav>
+  </header>
 </template>
--- NatterClient/src/components/navbar/NavFooter.vue
+++ NatterClient/src/components/navbar/NavFooter.vue
@@ -1,13 +1,28 @@
 <script setup>
+import { RouterLink } from 'vue-router';
+import { useUserStore } from '@/stores/userStore'
 
+const userStore = useUserStore()
+
+const year = new Date().getUTCFullYear()
 </script>
 
 <template>
-    <div class="absolute bottom-0 w-full h-10 border-t border-green-400">
-        <div class="flex items-center justify-center">
-            <p>2023 &copy;Natter</p>
+    <div class="dark:bg-dark bg-light text-dark dark:text-light border-t border-accent">
+        <div class="container divide-y dark:divide-light divide-dark">
+            <div class="flex items-center justify-between py-10 text-center">
+                <p>&copy; {{ year }} Natter</p>
+                <p>Connecting the World, One Chat at a Time</p>
+            </div>
+            <div class="flex items-center justify-center divide-x dark:divide-light divide-dark py-10">
+                <RouterLink :to="{name: 'home'}" class="px-5">Home</RouterLink>
+                <RouterLink :to="{name: 'chats'}" class="px-5">Natter Chat</RouterLink>
+                <RouterLink v-if="!userStore.isLoggedIn" :to="{name: 'auth'}" class="px-5">Login / Register</RouterLink>
+                <RouterLink v-else :to="{name: 'profile'}" class="px-5">Profile</RouterLink>
+            </div>
         </div>
     </div>
+
 </template>
 
 <style scoped>
