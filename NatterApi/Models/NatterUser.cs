660613f1d3cf2990bd2afce111b7443bd398e011
Samuel Ramesh <sampingu02@gmail.com>
2023-11-13 22:07:57 +0530

    Extend IdentityUser using NatterUser Model

--- NatterApi/Models/NatterUser.cs
+++ NatterApi/Models/NatterUser.cs
@@ -1,30 +1,20 @@
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 
 namespace NatterApi.Models;
 
-public class UserModel {
+public class NatterUser: IdentityUser {
 
-    public UserModel() {
+    public NatterUser() {
         ServersJoined = new();
     }
-
-    public int Id { get; set; }
-
+    
     public string? RefreshToken { get; set; }
 
-    [Required]
-    public string UserName { get; set; }
-
-    [Required]
-    public string Email { get; set; }
-
-    [Required]
     [JsonIgnore]
-    public string Password { get; set; }
-
-    [JsonIgnore]
-    public int PhoneNumber { get; set; }
+    public string PhoneNumber { get; set; }
 
     public string Country { get; set; }
 
