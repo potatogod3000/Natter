91b9b9eb78d40295a8de102254f368b92947fc2a
Samuel Ramesh <sampingu02@gmail.com>
2023-11-29 21:05:57 +0530

    Add reusable Modal Dialog Template component

---
+++ NatterClient/src/components/templates/ModalDialog.vue
@@ -0,0 +1,72 @@
+<template>
+    <TransitionRoot appear :show="props.isOpen" as="template">
+        <Dialog as="div" @close="$emit('cancel')" class="relative z-10">
+            <TransitionChild
+                as="template"
+                enter="duration-300 ease-out"
+                enter-from="opacity-0"
+                enter-to="opacity-100"
+                leave="duration-200 ease-in"
+                leave-from="opacity-100"
+                leave-to="opacity-0"
+            >
+                <div class="fixed inset-0 bg-black/25" />
+            </TransitionChild>
+  
+            <div class="fixed inset-0 overflow-y-auto">
+                <div class="flex min-h-full items-center justify-center p-4 text-center">
+                    <TransitionChild
+                    as="template"
+                    enter="duration-300 ease-out"
+                    enter-from="opacity-0 scale-95"
+                    enter-to="opacity-100 scale-100"
+                    leave="duration-200 ease-in"
+                    leave-from="opacity-100 scale-100"
+                    leave-to="opacity-0 scale-95"
+                    >
+                        <DialogPanel class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                            <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">
+                                <slot name="title">Title</slot>
+                            </DialogTitle>
+
+                            <div class="mt-2">
+                                <p class="text-sm text-gray-500">
+                                    <slot name="body">Body</slot>
+                                </p>
+                            </div>
+    
+                            <div class="flex gap-4 justify-center items-center mt-4">
+
+                                <slot name="button1">
+                                    <button type="button"
+                                        class="rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                    >
+                                        Button 1
+                                    </button>
+                                </slot>
+
+                                <slot name="button2">
+                                    <button type="button"
+                                        class="rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                    >
+                                        Button 2
+                                    </button>
+                                </slot>
+
+                            </div>
+                        </DialogPanel>
+                    </TransitionChild>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+  </template>
+  
+<script setup>
+import { TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle } from '@headlessui/vue'
+
+const props = defineProps({
+    isOpen: Boolean,
+})
+</script>
+  
