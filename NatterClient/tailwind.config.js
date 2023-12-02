122f23026332208a7d605ad40b47f96e25aaa0d8
Samuel Ramesh <sampingu02@gmail.com>
2023-12-03 00:06:22 +0530

    Styling improvements

--- NatterClient/tailwind.config.js
+++ NatterClient/tailwind.config.js
@@ -1,33 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js}"
-  ],
+    content: ["./index.html", "./src/**/*.{vue,js}"],
 
-  darkMode: "class",
-  
-  theme: {
-    extend: {
-      colors: {
-        "dark": "#1e1e20",
-        "dark-soft": "#252529",
-        "light": "#e3e3e5",
-        "light-soft": "#f6f6f7",
-        "accent": "#448aff",
-        "accent-light": "#82b1ff"
-      },
-      fontFamily: {
-        palanquin: ['Palanquin', 'sans-serif'],
-        montserrat: ['Montserrat', 'sans-serif'],
-      },
-      container: {
-        padding: "2rem",
-        center: true
-      }
+    darkMode: "class",
+
+    theme: {
+        extend: {
+            colors: {
+                dark: "#252525",
+                "dark-soft": "#333333",
+                light: "#d6ebff",
+                "light-soft": "#f2f9ff",
+                accent: "#3366ff",
+                "accent-light": "#5588ff",
+            },
+            fontFamily: {
+                palanquin: ["Palanquin", "sans-serif"],
+                montserrat: ["Montserrat", "sans-serif"],
+            },
+            container: {
+                padding: "2rem",
+                center: true,
+            },
+        },
     },
-  },
-  
-  plugins: [],
-}
+
+    plugins: [],
+};
