a5dcee5632751f5307b36db3a0c28974f8a1a347
Samuel Ramesh <sampingu02@gmail.com>
2024-08-24 23:56:03 +0530

    Adding routes

--- NatterClient/src/app/app.routes.ts
+++ NatterClient/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'chat',
+        loadComponent: () =>
+            import('./chat/chat.component').then(
+                (mod) => mod.ChatComponent
+            ),
+    },
+    {
+        path: 'profile',
+        loadComponent: () =>
+            import('./profile/profile.component').then(
+                (mod) => mod.ProfileComponent
+            ),
+    },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./auth/login/login.component').then(
+                (mod) => mod.LoginComponent
+            ),
+    },
+    {
+        path: 'register',
+        loadComponent: () =>
+            import('./auth/register/register.component').then(
+                (mod) => mod.RegisterComponent
+            ),
+    },
+];
