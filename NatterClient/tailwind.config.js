d01cdcba8cdc8df434ba99aeca2cad2d7527b15c
Samuel Ramesh <sampingu02@gmail.com>
2024-08-24 23:56:39 +0530

    Adding Tailwind styles and custom designs

--- NatterClient/tailwind.config.js
+++ NatterClient/tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,ts}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                dark: "#252525",
+                "dark-soft": "#333333",
+                light: "#d6ebff",
+                "light-soft": "#f2f9ff",
+                accent: "#5588ff",
+                "accent-light": "#77aaff",
+            },
+            fontFamily: {
+                palanquin: ["Palanquin", "sans-serif"],
+                montserrat: ["Montserrat", "sans-serif"],
+            },
+            container: {
+                padding: "2rem",
+                center: true,
+            },
+        },
+    },
+    plugins: [],
 };
